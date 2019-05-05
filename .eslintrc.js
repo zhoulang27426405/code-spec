2619d1c8855a14ef4a7cd4ee6c498f3b1dcd27e2
zhoulang <zhoulang@zhuanzhuan.com>
2019-05-05 19:11:36 +0800

    package修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,6 @@ module.exports = {
     node: true
   },
   extends: ['plugin:vue/essential', 'standard', 'plugin:prettier/recommended'],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
   rules: {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     eqeqeq: 'warn'
